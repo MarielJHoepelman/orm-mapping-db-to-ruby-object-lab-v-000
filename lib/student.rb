87367be3f9ecb8a3c8b7e148e4628c3a84685b22
Mariel Hoepelman <marielhoepelman@gmail.com>
2019-03-10 17:24:17 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -57,6 +57,7 @@ class Student
 
   def self.students_below_12th_grade
     sql = "SELECT * FROM students WHERE grade < 12"
+    
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end
